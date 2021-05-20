132ed6d57f74c9fd71d9376a31e65fa20d98ec25
Luis Cossío <luis.cossio@outlook.com>
2021-05-19 22:24:28 -0500

    refactor a estilo ejs

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// importing routes
+const indexRoutes = require('./routes/routeIndex');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname,'views/pages'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.set(express.static('public'));
+
+
+// routes
+app.use('/', indexRoutes);
+
+app.listen(app.get('port'), () => {
+    console.log('Listening on port: ' + app.get('port'));
+})
